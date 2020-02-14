038f9a532e5373450047ceb94cbd783ddf0d2c0d
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2020-02-14 11:50:29 +0800

    添加可插拔的监控配置

--- conf/setting.py
+++ conf/setting.py
@@ -8,6 +8,6 @@ PLUGINS_DICT = {
     'basic': 'src.plugins.basic.Basic',
     'cpu': 'src.plugins.cpu.Cpu',
     'disk': 'src.plugins.disk.Disk',
-    'memory': 'src.plugins.memory.Memory',
-    #'nic': 'src.plugins.nic.Nic',
+    # 'memory': 'src.plugins.memory.Memory',
+    'nic': 'src.plugins.nic.Nic',
 }
