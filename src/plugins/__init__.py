100bfea0ec8cbac5eb3a2f7ac7f78fab1f20103b
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2020-02-19 17:19:21 +0800

    调用各个执行命令process方法，并返回

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -7,37 +7,41 @@ import importlib
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from lib.config.settings import settings
+from lib.util.import_str import get_class
 
 
-class PluginsManager(object):
+# class PluginsManager(object):
+#
+#     def __init__(self, hostname=None):
+#         self.plugins_dict = settings.PLUGINS_DICT
+#
+#     # 管理配置文件种采集的插件
+#     def execute(self):
+#
+#         response = {}
+#         # 首先获取配置文件中 PLUGINS_DICT 循环获取
+#         for name, path in self.plugins_dict.items():
+#             cls = get_class(path)
+#             obj = cls()
+#             # 一个函数当成一个参数方式传给每个插件的process函数
+#             res = obj.process()
+#             response[name] = res
+#             # res = cls().process(self._cmd_run)
+#         return response
 
-    def __init__(self, hostname=None):
-        self.plugins_dict = settings.PLUGINS_DICT
-        self.mode = settings.ENGINE
-        self.hostname = hostname
+def get_server_info(handler, hostname=None):
+    # 根据配置获取相应的硬件信息
+    response = {}
+    # 首先获取配置文件中 PLUGINS_DICT 循环获取
+    for name, path in settings.PLUGINS_DICT.items():
+        cls = get_class(path)
+        obj = cls()
+        # 一个函数当成一个参数方式传给每个插件的process函数
+        res = obj.process(handler, hostname)
+        response[name] = res
+        # res = cls().process(self._cmd_run)
+    return response
 
-    # 管理配置文件种采集的插件
-    def execute(self):
-
-        response = {}
-        # 首先获取配置文件中 PLUGINS_DICT 循环获取
-        for k, v in self.plugins_dict.items():
-
-            module_name, class_name = v.rsplit('.', 1)
-
-            # 将一个包以字符串的形式导入
-            module_path = importlib.import_module(module_name)
-
-            # 将值的类导入并实例化，执行process方法
-            cls = getattr(module_path, class_name)
-            res = cls().process(self._cmd_run)
-            response[k] = res
-
-            # 一个函数当成一个参数方式传给每个插件的process函数
-            # res = cls().process(self._cmd_run)
-
-
-        return response
 
 
     # def _cmd_run(self, cmd):
